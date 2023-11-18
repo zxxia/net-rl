8818b253e3d02b3b9195f42b95ee1d5cede859cf
Zhengxu Xia <zxxia@uchicago.edu>
2023-11-18 11:48:44 -0600

    Add PCC Aurora and Aurora Monitor Interval

---
+++ src/simulator_new/cc/pcc/aurora/aurora.py
@@ -0,0 +1,91 @@
+from typing import List, Optional
+from simulator_new.cc import CongestionControl
+from simulator_new.cc.pcc.aurora.monitor_interval import MonitorInterval, MonitorIntervalHistory
+from simulator_new.constant import MSS
+
+
+def pcc_aurora_reward(tput_pkt_per_sec: float, delay_sec: float, loss: float,
+                      avg_bw_pkt_per_sec: Optional[float] = None,
+                      min_rtt_sec: Optional[float] = None) -> float:
+    """PCC Aurora reward. Anchor point 0.6Mbps
+    throughput: packets per second
+    delay: second
+    loss:
+    avg_bw: packets per second
+    """
+    # if avg_bw is not None and min_rtt is not None:
+    #     return 10 * 50 * throughput/avg_bw - 1000 * delay * 0.2 / min_rtt - 2000 * loss
+    if avg_bw_pkt_per_sec is not None:
+        return 10 * 50 * tput_pkt_per_sec/avg_bw_pkt_per_sec - 1000 * delay_sec - 2000 * loss
+    return 10 * tput_pkt_per_sec - 1000 * delay_sec - 2000 * loss
+
+class Aurora(CongestionControl):
+
+    MAX_RATE_BYTES_PER_SEC = 30000000  # 240Mbps
+    MIN_RATE_BYTES_PER_SEC = 7500  # 0.06Mbps
+
+    def __init__(self, model_path: str, history_len: int = 10,
+                 features: List[str] = ["sent latency inflation",
+                                        "latency ratio", "recv ratio"]) -> None:
+        super().__init__()
+        self.model_path = model_path
+        self.features = features
+        self.history_len = history_len
+
+        self.mi_duration_ms = 10
+        self.mi_end_ts_ms = 10
+        self.mi_history = MonitorIntervalHistory(history_len, features)
+        self.mi = MonitorInterval()
+        self.reward = 0
+
+    def on_pkt_sent(self, ts_ms, pkt):
+        self.mi.on_pkt_sent(ts_ms, pkt)
+
+    def on_pkt_acked(self, ts_ms, pkt):
+        self.mi.on_pkt_acked(ts_ms, pkt)
+
+    def tick(self, ts_ms):
+        if ts_ms >= self.mi_end_ts_ms:
+            self._on_mi_finish(ts_ms)
+            obs = self.get_obs()  # obtain the observation vector
+            # TODO: make decision here
+            self.apply_rate_delta(0)
+
+    def reset(self):
+        self.mi_duration_ms = 10
+        self.mi_end_ts_ms = 10
+        self.mi_history = MonitorIntervalHistory(self.history_len, self.features)
+        self.mi = MonitorInterval()
+        self.reward = 0
+
+    def apply_rate_delta(self, delta):
+        assert self.host
+        delta = float(delta)
+        if delta >= 0.0:
+            self.set_rate(self.host.pacing_rate_bytes_per_sec * (1.0 + delta))
+        else:
+            self.set_rate(self.host.pacing_rate_bytes_per_sec / (1.0 - delta))
+
+    def set_rate(self, pacing_rate_bytes_per_sec):
+        assert self.host
+        pacing_rate_bytes_per_sec = min(Aurora.MAX_RATE_BYTES_PER_SEC,
+                                        pacing_rate_bytes_per_sec)
+        pacing_rate_bytes_per_sec = max(Aurora.MIN_RATE_BYTES_PER_SEC,
+                                        pacing_rate_bytes_per_sec)
+        self.host.pacing_rate_bytes_per_sec = pacing_rate_bytes_per_sec
+
+    def get_obs(self):
+        return self.mi_history.as_array()
+
+    def _on_mi_finish(self, ts_ms):
+        # compute reward
+        tput = self.mi.get("recv rate")  # bytes/sec
+        lat = self.mi.get("avg latency")  # ms
+        loss = self.mi.get("loss ratio")
+        self.reward = pcc_aurora_reward(tput / MSS, lat, loss)
+
+        self.mi_duration_ms = lat  # set next mi duration
+        self.mi_end_ts_ms = ts_ms + self.mi_duration_ms
+
+        self.mi_history.step(self.mi) # append current mi to mi history
+        self.mi = MonitorInterval(conn_min_avg_lat_ms=self.mi.conn_min_latency_ms())  # create a new mi
---
+++ src/simulator_new/cc/pcc/aurora/monitor_interval.py
@@ -0,0 +1,182 @@
+import numpy as np
+
+class MonitorInterval:
+    # next_mi_id = 0
+
+
+    def __init__(self,
+                 bytes_sent=0,
+                 bytes_acked=0,
+                 bytes_lost=0,
+                 send_start_ts_ms=0,
+                 send_end_ts_ms=0,
+                 recv_start_ts_ms=0,
+                 recv_end_ts_ms=0,
+                 rtt_ms_samples=[],
+                 qdelay_ms_samples=[],
+                 conn_min_avg_lat_ms=0) -> None:
+        self.bytes_sent = bytes_sent
+        self.bytes_acked = bytes_acked
+        self.bytes_lost = bytes_lost
+        self.send_start_ts_ms = send_start_ts_ms
+        self.send_end_ts_ms = send_end_ts_ms
+        self.recv_start_ts_ms = recv_start_ts_ms
+        self.recv_end_ts_ms = recv_end_ts_ms
+        self.rtt_ms_samples = rtt_ms_samples
+        self.qdelay_ms_samples = qdelay_ms_samples
+        self.conn_min_avg_lat_ms = conn_min_avg_lat_ms
+        # self.mi_id = MonitorInterval.next_mi_id
+        # MonitorInterval.next_mi_id += 1
+
+        self.metric_map = {
+            "send rate": (self.send_rate_bytes_per_sec, 0.0, 1e9, 1e7),
+            "recv rate": (self.recv_rate_bytes_per_sec, 0.0, 1e9, 1e7),
+            "recv dur": (self.recv_dur_ms, 0.0, 100.0, 1),
+            "send dur": (self.send_dur_ms, 0.0, 100.0, 1),
+            "avg latency": (self.avg_latency_ms, 0.0, 100.0, 1),
+            "avg queue delay": (self.avg_queue_delay_ms, 0.0, 100.0, 1),
+            "loss ratio": (self.loss_ratio, 0.0, 1.0, 1),
+            "ack latency inflation": (self.ack_latency_inflation, -1.0, 10.0, 1),
+            "sent latency inflation": (self.sent_latency_inflation, -1.0, 10.0, 1),
+            "conn min latency": (self.conn_min_latency_ms, 0.0, 100.0, 1),
+            "latency increase": (self.latency_increase_ms, 0.0, 100.0, 1),
+            "latency ratio": (self.latency_ratio, 1.0, 10000.0, 1),
+            "send ratio": (self.send_ratio, 0.0, 1000.0, 1),
+            "recv ratio": (self.recv_ratio, 0.0, 1000.0, 1)}
+
+    def get(self, feature):
+        # print(self.metric_map[feature])
+        func, min_val, max_val, scale = self.metric_map[feature]
+        return func() / scale
+
+    # Convert the observation parts of the monitor interval into a numpy array
+    def as_array(self, features):
+        scale = 1.0
+        return np.array([self.get(f) / scale for f in features])
+
+    def on_pkt_sent(self, ts_ms, pkt):
+        if self.bytes_sent == 0:
+            # TODO: double check this line
+            self.send_start_ts_ms = ts_ms
+        self.send_end_ts_ms = ts_ms
+        self.bytes_sent += pkt.size_bytes
+
+    def on_pkt_acked(self, ts_ms, pkt):
+        if self.bytes_acked == 0:
+            self.recv_start_ts_ms = ts_ms
+        self.recv_end_ts_ms = ts_ms
+        self.bytes_acked += pkt.size_bytes
+        self.rtt_ms_samples.append(pkt.rtt_ms())
+        # TODO: get qdelay ms from ack pkt
+
+    def on_pkt_lost(self, ts_ms, pkt):
+        raise NotImplementedError
+
+    def recv_dur_ms(self):
+        return self.recv_end_ts_ms - self.recv_start_ts_ms
+
+    def recv_rate_bytes_per_sec(self):
+        dur_sec = self.recv_dur_ms() / 1000
+        if dur_sec > 0.0:
+            return self.bytes_acked / dur_sec
+        return 0
+
+    def avg_latency_ms(self):
+        if len(self.rtt_ms_samples) > 0:
+            return np.mean(self.rtt_ms_samples)
+        return 0.0
+
+    def avg_queue_delay_ms(self):
+        if len(self.qdelay_ms_samples) > 0:
+            return np.mean(self.qdelay_ms_samples)
+        return 0.0
+
+    def send_dur_ms(self):
+        return self.send_end_ts_ms - self.send_start_ts_ms
+
+    def send_rate_bytes_per_sec(self):
+        dur_sec = self.send_dur_ms()
+        if dur_sec > 0.0:
+            return self.bytes_sent / dur_sec
+        return 0.0
+
+    def loss_ratio(self):
+        # This does not make sense
+        if self.bytes_lost + self.bytes_acked > 0:
+            return self.bytes_lost / (self.bytes_lost + self.bytes_acked)
+        return 0.0
+
+    def latency_increase_ms(self):
+        half = int(len(self.rtt_ms_samples) / 2)
+        if half >= 1:
+            return np.mean(self.rtt_ms_samples[half:]) - np.mean(self.rtt_ms_samples[:half])
+        return 0.0
+
+    def ack_latency_inflation(self):
+        dur_ms = self.recv_dur_ms()
+        lat_inc_ms = self.latency_increase_ms()
+        if dur_ms > 0.0:
+            return lat_inc_ms / dur_ms
+        return 0.0
+
+    def sent_latency_inflation(self):
+        dur_ms = self.send_dur_ms()
+        lat_inc_ms = self.latency_increase_ms()
+        if dur_ms > 0.0:
+            return lat_inc_ms / dur_ms
+        return 0.0
+
+    def conn_min_latency_ms(self):
+        avg_lat_ms = self.avg_latency_ms()
+        if avg_lat_ms > 0 and self.conn_min_avg_lat_ms > 0:
+            self.conn_min_avg_lat_ms = min(self.conn_min_avg_lat_ms, avg_lat_ms)
+        elif self.conn_min_avg_lat_ms == 0:
+            self.conn_min_avg_lat_ms = avg_lat_ms
+        return self.conn_min_avg_lat_ms
+
+    def send_ratio(self):
+        thpt = self.recv_rate_bytes_per_sec()
+        send_rate = self.send_rate_bytes_per_sec()
+        if (thpt > 0.0) and (send_rate < 1000.0 * thpt):
+            return send_rate / thpt
+        # elif thpt == 0:
+        #     return 2 #send_rate / 0.1
+        return 1.0
+
+    def recv_ratio(self):
+        thpt = self.recv_rate_bytes_per_sec()
+        send_rate = self.send_rate_bytes_per_sec()
+        if send_rate == 0:
+            return 1.0
+        return thpt / send_rate
+
+    def latency_ratio(self):
+        min_lat = self.conn_min_latency_ms()
+        cur_lat = self.avg_latency_ms()
+        if min_lat > 0.0:
+            return cur_lat / min_lat
+        return 1.0
+
+
+class MonitorIntervalHistory():
+    def __init__(self, length, features):
+        self.length = length
+        self.features = features
+        self.values = []
+        # self.sender_id = sender_id
+        for _ in range(0, length):
+            self.values.append(MonitorInterval())
+
+    def step(self, new_mi):
+        self.values.pop(0)
+        self.values.append(new_mi)
+
+    def as_array(self):
+        arrays = []
+        for mi in self.values:
+            arrays.append(mi.as_array(self.features))
+        arrays = np.array(arrays).flatten()
+        return arrays
+
+    def back(self):
+        return self.values[-1]
