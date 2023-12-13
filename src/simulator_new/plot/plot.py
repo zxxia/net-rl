a6ba07b8f6d0b5d60e5c66aa8b2d1726189544de
Zhengxu Xia <zxxia@uchicago.edu>
2023-12-12 18:52:52 -0600

    Merge decoder log plot and pkt plot into one

--- src/simulator_new/plot/plot.py
+++ src/simulator_new/plot/plot.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 from simulator_new.cc.pcc.aurora.aurora import pcc_aurora_reward
-from simulator_new.constant import BITS_PER_BYTE, MSS
+from simulator_new.constant import MSS
 from simulator_new.stats_recorder import PacketLog
 from simulator_new.trace import Trace
 
@@ -17,9 +17,9 @@ def plot_mi_log(trace: Optional[Trace], log_file: str, save_dir: str, cc: str):
     df = pd.read_csv(log_file)
     assert isinstance(df, pd.DataFrame)
     ts_sec = df['timestamp_ms'] / 1e3
-    recv_rate_mbps = df['recv_rate_byte_per_sec'] * BITS_PER_BYTE / 1e6
+    recv_rate_mbps = df['recv_rate_byte_per_sec'] * 8 / 1e6
     avg_recv_rate_mbps = recv_rate_mbps.mean()
-    send_rate_mbps = df['send_rate_byte_per_sec'] * BITS_PER_BYTE / 1e6
+    send_rate_mbps = df['send_rate_byte_per_sec'] * 8 / 1e6
     send_recv_rate_mbps = send_rate_mbps.mean()
     avg_lat_ms = df['latency_ms'].mean()
     avg_loss_ratio = df['loss_ratio'].mean()
@@ -66,9 +66,9 @@ def plot_mi_log(trace: Optional[Trace], log_file: str, save_dir: str, cc: str):
     axes[2].set_ylim(0, 1)
 
     avg_reward_mi = pcc_aurora_reward(
-            avg_recv_rate_mbps / BITS_PER_BYTE / MSS,
+            avg_recv_rate_mbps / 8 / MSS,
             avg_lat_ms / 1e3, avg_loss_ratio,
-            avg_bw * 1e6 / BITS_PER_BYTE / MSS, min_rtt)
+            avg_bw * 1e6 / 8 / MSS, min_rtt)
 
     axes[3].plot(ts_sec, df['reward'],
                  label='rewards avg {:.3f}'.format(avg_reward_mi))
@@ -100,7 +100,7 @@ def plot_mi_log(trace: Optional[Trace], log_file: str, save_dir: str, cc: str):
     plt.close()
 
 
-def plot_pkt_log(trace, log_file, save_dir, cc):
+def plot_pkt_log(trace, log_file, save_dir, cc, decoder_log: Optional[str] = None):
     pkt_log = PacketLog.from_log_file(log_file, 500)
     sending_rate_ts_sec, sending_rate_mbps = pkt_log.get_sending_rate_mbps()
     tput_ts_sec, tput_mbps = pkt_log.get_throughput_mbps()
@@ -112,14 +112,67 @@ def plot_pkt_log(trace, log_file, save_dir, cc):
     avg_lat = pkt_log.get_avg_rtt_ms()
     # reward = pkt_log.get_reward("", None)
     # normalized_reward = pkt_log.get_reward("", trace)
-    fig, axes = plt.subplots(2, 1, figsize=(6, 8))
+    ts_max = min([trace.timestamps[-1], sending_rate_ts_sec[-1], tput_ts_sec[-1]])
+
+    if decoder_log:
+        df = pd.read_csv(decoder_log)
+        fig, axes = plt.subplots(5, 1, figsize=(6, 12))
+        ax = axes[2]
+        ax.plot(df['timestamp_ms'] / 1000, df['frame_loss_rate'], 'o-', ms=2, color='C0')
+        ax.set_xlabel('Time(s)')
+        ax.set_ylabel('Frame loss rate')
+        ax.set_xlim(0, ts_max)
+        ax.set_ylim(0, 1)
+        ax2 = ax.twiny()
+        ax2.set_xlabel('Frame id')
+        nticks = len(ax.get_xticks())
+        step_len = int((len(df['frame_id']) - 1) / (nticks - 1))
+        ax2_xticks = [df['timestamp_ms'].iloc[i * step_len] / 1000 for i in range(nticks)]
+        ax2_xticklabels = [str(df['frame_id'].iloc[i * step_len]) for i in range(nticks)]
+        ax2.set_xbound(ax.get_xbound())
+        ax2.set_xticks(ax2_xticks)
+        ax2.set_xticklabels(ax2_xticklabels)
+        ax2.set_xlim(0, ts_max)
+
+        ax = axes[3]
+        avg_ssim = df['ssim'].mean()
+        ax.plot(df['timestamp_ms'] / 1000, df['ssim'], 'o-', ms=2, color='C1',
+                label=f'avg = {avg_ssim:.3f}')
+        ax.set_xlabel('Time(s)')
+        ax.set_ylabel('SSIM')
+        ax.set_xlim(0, ts_max)
+        ax.legend()
+        ax2 = ax.twiny()
+        ax2.set_xlabel('Frame id')
+        ax2.set_xbound(ax.get_xbound())
+        ax2.set_xticks(ax2_xticks)
+        ax2.set_xticklabels(ax2_xticklabels)
+        ax2.set_xlim(0, ts_max)
+
+        ax = axes[4]
+        frame_delay_ms = df['frame_decode_ts_ms'] - df['frame_encode_ts_ms']
+        avg_frame_delay_ms = frame_delay_ms.mean()
+        ax.plot(df['timestamp_ms'] / 1000, frame_delay_ms, 'o-', ms=2,
+                color='C2', label=f'avg = {avg_frame_delay_ms:.2f}ms')
+        ax.set_xlabel('Time(s)')
+        ax.set_xlim(0, ts_max)
+        ax.set_ylabel('Frame delay(ms)')
+        ax.legend()
+        ax2 = ax.twiny()
+        ax2.set_xlabel('Frame id')
+        ax2.set_xbound(ax.get_xbound())
+        ax2.set_xticks(ax2_xticks)
+        ax2.set_xticklabels(ax2_xticklabels)
+        ax2.set_xlim(0, ts_max)
+    else:
+        fig, axes = plt.subplots(2, 1, figsize=(6, 8))
     axes[0].plot(tput_ts_sec, tput_mbps, "-o", ms=2,  # drawstyle='steps-post',
                  label='tput, avg {:.3f}Mbps'.format(avg_tput_mbps))
     axes[0].plot(sending_rate_ts_sec, sending_rate_mbps, "-o", ms=2,  # drawstyle='steps-post',
                  label='send rate, avg {:.3f}Mbps'.format(avg_sending_rate_mbps))
     if trace is not None:
         axes[0].plot(trace.timestamps, trace.bandwidths, "-o", ms=2,  # drawstyle='steps-post',
-                     label='bandwidth, avg {:.3f}Mbps'.format(np.mean(trace.bandwidths)))
+                     label='bw, avg {:.3f}Mbps'.format(np.mean(trace.bandwidths)))
         queue_size = trace.queue_size
         trace_random_loss = trace.loss_rate
         delay_noise = trace.delay_noise
@@ -129,7 +182,6 @@ def plot_pkt_log(trace, log_file, save_dir, cc):
         delay_noise = "N/A"
         # axes[0].plot(np.arange(30), np.ones_like(np.arange(30)) * 6, "-o", ms=2,  # drawstyle='steps-post',
         #              label='bandwidth, avg {:.3f}Mbps'.format(6))
-    ts_max = min([trace.timestamps[-1], sending_rate_ts_sec[-1], tput_ts_sec[-1]])
     axes[0].legend()
     axes[0].set_xlabel("Time(s)")
     axes[0].set_ylabel("Rate(Mbps)")
@@ -152,8 +204,7 @@ def plot_pkt_log(trace, log_file, save_dir, cc):
     axes[1].set_ylabel("Latency(ms)")
     axes[1].set_title('{} loss={:.3f}, rand loss={:.3f}, queue cap={}pkt'.format(
         cc, pkt_loss_rate, trace_random_loss, queue_size))
-    axes[1].set_xlim(0, rtt_ts_sec[-1])
-    # axes[1].set_ylim(0, )
+    axes[1].set_xlim(0, ts_max)
 
     fig.tight_layout()
     if save_dir:
