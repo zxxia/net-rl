c8e2d1797ce4792334df99a440225e5da76510b9
Zhengxu Xia <zxxia@uchicago.edu>
2024-01-25 14:49:07 -0600

    Update network trace collection and plot scripts

--- src/scripts/capture_traffic.sh
+++ src/scripts/capture_traffic.sh
@@ -6,15 +6,19 @@
 SERVER=13.52.163.96
 PORT=5001
 DURATION=300
-SAVE_DIR=results/pcap_traces/blackbox-aws-test
+SAVE_DIR=results/pcap_traces/blackbox-aws-test-campus
 
 mkdir -p $SAVE_DIR
 
-cur_t=$(date +%s%N | cut -b1-13)
-ssh $SERVER "iperf -s -D"
-iperf -t $DURATION -c $SERVER -p $PORT -i 5 > ${SAVE_DIR}/iperf_output_${cur_t}.log &
-sudo timeout ${DURATION}s tcpdump tcp port $PORT -s 80 -w ${SAVE_DIR}/tcp_${cur_t}.pcap
-ssh $SERVER "pkill -f iperf"
+# cur_t=$(date +%s%N | cut -b1-13)
+for i in $(seq 1 20); do
+    cur_t=$(date +%Y%m%d%H%m%S)
+    ssh $SERVER "iperf -s -D"
+    timeout ${DURATION} ./src/scripts/run_ss.sh ${SAVE_DIR}/ss_output_${cur_t}.log &
+    iperf -t $DURATION -c $SERVER -p $PORT -i 5 > ${SAVE_DIR}/iperf_output_${cur_t}.log &
+    sudo timeout ${DURATION}s tcpdump tcp port $PORT -s 80 -w ${SAVE_DIR}/tcp_${cur_t}.pcap
+    ssh $SERVER "pkill -f iperf"
+done
 
 # sudo tcpdump udp # -i wlp1s0 quic
 # sudo timeout 30s tcpdump -i wlp1s0 -w results/pcap_traces/webrtc_${cur_t}.pcap
