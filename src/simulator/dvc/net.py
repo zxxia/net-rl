7eaf2bc39315fc560e7fca06b456ea666e8b8c8b
Zhengxu Xia <zxxia@uchicago.edu>
2024-07-18 16:43:05 -0500

    Always load AE models on Nvidia GPU

--- src/simulator/dvc/net.py
+++ src/simulator/dvc/net.py
@@ -26,7 +26,7 @@ def save_model(model, iter):
 
 def load_model(model, f):
     with open(f, 'rb') as f:
-        pretrained_dict = torch.load(f, map_location=torch.device('cpu'))
+        pretrained_dict = torch.load(f)
         model_dict = model.state_dict()
         pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
         model_dict.update(pretrained_dict)
