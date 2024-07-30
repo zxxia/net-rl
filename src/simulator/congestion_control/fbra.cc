361fd7385cd572c176a138d9eae16498d2b623e7
Zhengxu Xia <zxxia@uchicago.edu>
2024-07-30 14:15:12 -0500

    Set minimum sending rate to fbra

--- src/simulator/congestion_control/fbra.cc
+++ src/simulator/congestion_control/fbra.cc
@@ -79,10 +79,10 @@ void FBRA::OnPktRcvd(const Packet* pkt) {
     const auto p80_owd = percentile(owd_history_, 80.0);
     const auto corr_owd_low = owd_ms / p40_owd;
     const auto corr_owd_high = owd_ms / p80_owd;
-    stream_ << now.ToMicroseconds() << "," << p40_owd << "," << p80_owd << ","
-            << static_cast<int>(state_) << "," << fec_encoder_->IsEnabled()
-            << "," << fec_interval_ << "," << corr_owd_low << ","
-            << corr_owd_high << std::endl;
+    stream_ << now.ToMicroseconds() << "," << rate_.ToBps() << "," << p40_owd
+            << "," << p80_owd << "," << static_cast<int>(state_) << ","
+            << fec_encoder_->IsEnabled() << "," << fec_interval_ << ","
+            << corr_owd_low << "," << corr_owd_high << std::endl;
     if (!enabled_) {
       return;
     }
@@ -223,7 +223,8 @@ void FBRA::Probe(const double losses, const double recent_losses,
       //          << rate_.ToMbps() << " to "
       //          << (rate_ * (1.0 / (1.0 - fec_encoder_->GetRate()))).ToMbps()
       //          << " probe->up\n";
-      rate_ = rate_ * (1.0 / (1.0 - fec_encoder_->GetRate()));
+      rate_ = std::max(rate_ * (1.0 / (1.0 - fec_encoder_->GetRate())),
+                       Rate::FromKbps(MIN_RATE_KBPS));
       fec_encoder_->Disable();
     }
   }
@@ -243,8 +244,8 @@ void FBRA::Undershoot() {
   // rate_ = rate_ - (rate_ - goodput) * 1.0;
   // rate_ = rate_ * 0.9;
   // std::cout << rate_.ToMbps() << " to ";
-  rate_ = rate_ * 0.85;
   // std::cout << rate_.ToMbps() << std::endl;
+  rate_ = std::max(rate_ * 0.85, Rate::FromKbps(MIN_RATE_KBPS));
 }
 
 void FBRA::BounceBack() {
@@ -254,8 +255,9 @@ void FBRA::BounceBack() {
   } else {
     // std::cout <<Clock::GetClock().Now().ToSeconds() << " bounce rate " <<
     // rate_.ToMbps() << " to ";
-    rate_ = goodput_during_undershoot_ * 0.9;
     // std::cout << rate_.ToMbps() << std::endl;
+    rate_ = std::max(goodput_during_undershoot_ * 0.9,
+                     Rate::FromKbps(MIN_RATE_KBPS));
   }
   state_ = FBRAState::STAY;
   enabled_ = true;
